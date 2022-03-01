13c5b59338a64fb0918fdcc930ac4c7936f737a5
Esteban D. Espinoza <espinoza.e@northeastern.edu>
2022-03-01 02:20:19 -0500

    Added RNDraw for stylus input

--- amadeus-ui/App.js
+++ amadeus-ui/App.js
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-
-import Canvas from './components/Canvas';
+import { Canvas } from '@benjeau/react-native-draw';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Amadeus Test</Text>
+      <Text style={styles.compTitle}>Amadeus Test</Text>
       <ImageBackground 
         source={require('./assets/staves.png')}
         style={{ width: "95%", height: "95%", alignSelf: "center" }}
       >
-        {/* <Canvas /> */}
+        <Canvas style={styles.canvas}/>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -19,10 +18,21 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  compTitle: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'black',
+    fontSize: 48,
+    // fontFamily: 'serif',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  canvas: {
+    backgroundColor: 'transparent',
+  }
 });

a7ca1e3f56bf46d7bbfc687f4de02999a12fc9f5
Esteban D. Espinoza <espinoza.e@northeastern.edu>
2024-04-14 00:18:53 -0400

    Cleaner folder structure, CloudFormation YAML added

---
+++ server/src/tensorflow/data/HOMUS.py
@@ -0,0 +1,14 @@
+from omrdatasettools import Downloader, OmrDataset
+
+downloader = Downloader()
+downloader.download_and_extract_dataset(OmrDataset.Homus_V2, "data")
+
+from omrdatasettools import HomusImageGenerator
+
+HomusImageGenerator.create_images(raw_data_directory="data",
+                                  destination_directory="HOMUS_labeled",
+                                  stroke_thicknesses=[3],
+                                  canvas_width=96,
+                                  canvas_height=192,
+                                  staff_line_spacing=14,
+                                  staff_line_vertical_offsets=None)
